16b9a81839cf325c017efe2d019e3d1e075c3c14
Arshiya <arshiya1383.mk@gmail.com>
2022-07-06 13:57:37 +0430

    add comments in index.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+//connection with html tags
 const bars = document.querySelectorAll(".bars__ul>li>span");
 const prices = document.querySelectorAll(".bars__ul>li>i");
 const date = new Date();
+//spilit the date string 
 const dateArr=date.toString().split(" ")
 const currentDay=dateArr[0].toLowerCase()
 
@@ -35,26 +37,30 @@ const dateJson = [
     amount: 25.48,
   },
 ];
+// retrive the index of the current day object
 let indexOfDay;
-const objectOfCurrentDay=dateJson.filter((dayAndAmount,i)=>{
+dateJson.filter((dayAndAmount,i)=>{
      if(dayAndAmount.day==currentDay){
         indexOfDay=i
-        return dayAndAmount
+        
      }
 })
-
+// add unhover class to all i tags
 prices.forEach((amount) => {
   amount.classList.add("bars-unhover");
 });
+//give span or bars background and height
 bars.forEach((bar,index) => {
+  //give blue background to current day
    if(index==indexOfDay){
         bar.style.backgroundColor="hsl(186, 34%, 60%)"
    }
+   //determine height of the bars
    if(dateJson[index].amount<55){
      bar.style.height=2*(dateJson[index].amount)+"px"
 
    }
-
+   //insert amount to all tha i tags 
    const amount=bar.nextElementSibling
    amount.innerText=dateJson[index].amount
 
@@ -63,7 +69,7 @@ bars.forEach((bar,index) => {
 
 
 
-
+   // do some stuff for perfect animation
   bar.addEventListener("mouseout", () => {
     const price = bar.nextElementSibling;
     price.classList.remove("bars-hover");
